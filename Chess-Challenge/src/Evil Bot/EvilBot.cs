d0e05ddd77dc73a9f8062b9321f22af34c4a9c27
Rafael Bayer <rafibayer7@gmail.com>
2023-07-22 17:12:01 -0700

    something is weird but it's reasonably good

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,54 +1,113 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ChessChallenge.Example
+// MyBot @a1b14cd3d706ba672e93190b7bec50d9fb770f8b
+public class EvilBot : IChessBot
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
-    public class EvilBot : IChessBot
+    /*
+     * Token saving:
+     * use and pass lambdas with var type instead of function signature
+     *  var f = (...) => ... 
+     *  
+     *  implicit ctors everywhere
+     *      T thing = new();
+     *      
+     *  repeated consts
+     *      double.NegativeInfinity => double inf = double.NegativeInfinity
+     *      saves after enough uses
+     */
+
+    static int[] weights = new int[]
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        0,   // None,   // 0
+        100, // Pawn,   // 1
+        300, // Knight, // 2
+        500, // Bishop, // 3
+        500, // Rook,   // 4
+        900, // Queen,  // 5
+        0,   // King    // 6
+    };
 
-        public Move Think(Board board, Timer timer)
+    Random rng = new();
+
+    public Move Think(Board board, Timer timer)
+    {
+        Move bestMove = Move.NullMove;
+        double bestScore = double.NegativeInfinity;
+
+        string me = board.IsWhiteToMove ? "White" : "Black";
+
+        foreach (var nextMove in GetMoves(board))
         {
-            Move[] allMoves = board.GetLegalMoves();
-
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
-
-            foreach (Move move in allMoves)
+            board.MakeMove(nextMove);
+            // pick the move that is worst for the next player
+            double eval = -Negamax(board, 4, double.NegativeInfinity, double.PositiveInfinity, timer);
+            if (eval > bestScore)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
-                    break;
-                }
-
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
-                }
+                Console.WriteLine($"{me} found better move with score {eval}");
+                bestMove = nextMove;
+                bestScore = eval;
             }
 
-            return moveToPlay;
+            board.UndoMove(nextMove);
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
-        {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
-        }
+        Console.WriteLine("\n========");
+        return bestMove;
     }
+
+    // https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning
+    double Negamax(
+        Board board,
+        int depth,
+        double alpha,
+        double beta,
+        Timer timer)
+    {
+        if (depth == 0 || board.IsInCheckmate() || board.IsDraw() || timer.MillisecondsElapsedThisTurn > 250)
+            return Evaluate(board);
+
+        double value = double.NegativeInfinity;
+        foreach (var nextMove in GetMoves(board))
+        {
+            board.MakeMove(nextMove);
+            try
+            {
+                // 0.9 discount factor, near states are better than far states
+                value = Math.Max(value, 0.9 * -Negamax(board, depth - 1, -beta, -alpha, timer));
+                alpha = Math.Max(alpha, value);
+
+                if (alpha >= beta)
+                {
+                    break;
+                }
+            }
+            finally
+            {
+                board.UndoMove(nextMove);
+            }
+        }
+
+        return value;
+    }
+
+    // relative to whos turn it is
+    public double Evaluate(Board board)
+    {
+        if (board.IsInCheckmate())
+            return -10000;
+
+        return board
+            .GetAllPieceLists()
+            .SelectMany(pl => pl)
+            .Select(p => weights[(int)p.PieceType] * Sign(p.IsWhite == board.IsWhiteToMove))
+            .Sum() + (Sign(board.IsInCheck()) * -500);
+    }
+
+    int Sign(bool white) => white ? 1 : -1;
+
+    IEnumerable<Move> GetMoves(Board board) => board.GetLegalMoves().OrderBy(_ => rng.NextDouble());
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -19,7 +19,7 @@ public class MyBot : IChessBot
      *      saves after enough uses
      */
 
-    static int[] weights = new int[]
+    int[] weights = new int[]
     {
         0,   // None,   // 0
         100, // Pawn,   // 1
@@ -31,42 +31,72 @@ public class MyBot : IChessBot
     };
 
     Random rng = new();
+    
+    // zobrist -> value, flag {-1 LOWER, 0 EXACT, 1 UPPER}
+    Dictionary<ulong, (double, int)> transpositionTable = new();
 
     public Move Think(Board board, Timer timer)
     {
         Move bestMove = Move.NullMove;
         double bestScore = double.NegativeInfinity;
-
         string me = board.IsWhiteToMove ? "White" : "Black";
 
-        foreach (var nextMove in GetMoves(board))
+        for (int ply = 1; timer.MillisecondsElapsedThisTurn < 500; ply++) 
         {
-            board.MakeMove(nextMove);
-            // pick the move that is worst for the next player
-            double eval = -Negamax(board, 4, double.NegativeInfinity, double.PositiveInfinity, timer);
-            if (eval > bestScore)
+            double bestScoreThisPly = double.NegativeInfinity;
+            Move bestMoveThisPly = Move.NullMove;
+
+            foreach (var nextMove in GetMoves(board))
             {
-                Console.WriteLine($"{me} found better move with score {eval}");
-                bestMove = nextMove;
-                bestScore = eval;
+                board.MakeMove(nextMove);
+                // pick the move that is worst for the next player
+                double eval = -Negamax(board, ply, double.NegativeInfinity, double.PositiveInfinity);
+                if (eval >= bestScore)
+                {
+                    Console.WriteLine($"{me} found better move with score {eval} on ply {ply}");
+                    bestMove = nextMove;
+                    bestScore = eval;
+                }
+
+                if (eval >= bestScoreThisPly)
+                {
+                    bestScoreThisPly = eval;
+                    bestMoveThisPly = nextMove;
+                }
+
+                board.UndoMove(nextMove);
             }
 
-            board.UndoMove(nextMove);
+            Console.WriteLine($"best this ply: {bestMoveThisPly} for {bestScore} on ply {ply}");
         }
-
         Console.WriteLine("\n========");
         return bestMove;
     }
 
-    // https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning
+    // https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning_and_transposition_tables
     double Negamax(
         Board board,
         int depth,
         double alpha,
-        double beta,
-        Timer timer)
+        double beta)
     {
-        if (depth == 0 || board.IsInCheckmate() || board.IsDraw() || timer.MillisecondsElapsedThisTurn > 1000)
+        double originalAlpha = alpha;
+
+        if (transpositionTable.TryGetValue(board.ZobristKey, out var ttEntry))
+        {
+            switch (ttEntry.Item2) {
+                case 0:
+                    return ttEntry.Item1;
+                case -1:
+                    alpha = Math.Max(alpha, ttEntry.Item1);
+                    break;
+                case 1:
+                    beta = Math.Max(beta, ttEntry.Item1);
+                    break;
+            }
+        }    
+
+        if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
             return Evaluate(board);
 
         double value = double.NegativeInfinity;
@@ -75,8 +105,7 @@ public class MyBot : IChessBot
             board.MakeMove(nextMove);
             try
             {
-                // 0.9 discount factor, near states are better than far states
-                value = Math.Max(value, 0.9 * -Negamax(board, depth - 1, -beta, -alpha, timer));
+                value = Math.Max(value, -Negamax(board, depth - 1, -beta, -alpha));
                 alpha = Math.Max(alpha, value);
 
                 if (alpha >= beta)
@@ -90,20 +119,30 @@ public class MyBot : IChessBot
             }
         }
 
+        int ttFlag = 0;
+        if (value <= originalAlpha)
+            ttFlag = 1;
+        else if (value >= beta)
+            ttFlag = -1;
+
+        transpositionTable[board.ZobristKey] = (value, ttFlag);
+
         return value;
     }
 
-    // relative to whos turn it is
+    // relative to whos turn it is, high = good, low = bad
     public double Evaluate(Board board)
     {
+        // checkmake is always back on your turn
         if (board.IsInCheckmate())
             return -10000;
 
+        // material advantage, penalty for being in check
         return board
             .GetAllPieceLists()
             .SelectMany(pl => pl)
             .Select(p => weights[(int)p.PieceType] * Sign(p.IsWhite == board.IsWhiteToMove))
-            .Sum() + (Sign(board.IsInCheck()) * -500);
+            .Sum() + (Sign(board.IsInCheck()) * -200);
     }
 
     int Sign(bool white) => white ? 1 : -1;
